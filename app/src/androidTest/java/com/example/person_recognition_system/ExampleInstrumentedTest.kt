b92455ae0dd2fe9e92b8c10b468b814b3bd80e45
ThatMadMf <maxdarksiders@gmail.com>
2022-10-05 16:27:25 +0300

    Rename layouts & settings

--- app/src/androidTest/java/com/example/person_recognition_system/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/person_recognition_system/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.facerecognitionsystemapplication", appContext.packageName)
+        assertEquals("com.example.person_recognition_system", appContext.packageName)
     }
 }
